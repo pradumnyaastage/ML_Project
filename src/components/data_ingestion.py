f77dde5ca6b928852ca0a1f90b62c7496ffb47e3
Pradumnya_astage <pradumnyaastage98@gmail.com>
2024-04-26 15:06:40 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass
 class DataingestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -17,7 +21,7 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataingestionConfig()
         
-    def inititate_data_ingestion(self):
+    def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or compinent")
         try:
             df=pd.read_csv("notebook\data\stud.csv")
@@ -43,11 +47,15 @@ class DataIngestion:
             }         
         
         except Exception as e:
-            raise CustomerExecption(e,sys)
+            raise CustomException(e,sys)
         
 
 if __name__=="__main__" :
     obj=DataIngestion()
-    obj.inititate_data_ingestion()
+    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
         
 
